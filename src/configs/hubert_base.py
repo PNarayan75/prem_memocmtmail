f523380a83787116be9d0e65281795a6558dc297
Pranav Kumar <99776807+PNarayan75@users.noreply.github.com>
2025-10-28 22:00:16 +0530

    Refactor Config class and update parameters

--- src/configs/hubert_base.py
+++ src/configs/hubert_base.py
@@ -1,28 +1,6 @@
 from configs.base import Config as BaseConfig
 
-# class Config(BaseConfig):
-#     def __init__(self, **kwargs):
-#         super(Config, self).__init__(**kwargs)
-#         self.add_args()
-#         for key, value in kwargs.items():
-#             setattr(self, key, value)
-        
-#     def add_args(self, **kwargs):
-#         # self.batch_size = 1
-#         # self.num_epochs = 3
-#         self.batch_size = 8
-#         self.num_epochs = 30  
-#         self.learning_rate = 1e-4 #new add kiya h
-#         self.audio_unfreeze = True
-#         self.text_unfreeze = True
-#         self.dropout = 0.1
-#         self.loss_type = "CrossEntropyLoss" 
-#         self.checkpoint_dir = "/kaggle/working/checkpoints/IEMOCAP"
-from configs.base import Config as BaseConfig
-
-
 class Config(BaseConfig):
-    # Base
     def __init__(self, **kwargs):
         super(Config, self).__init__(**kwargs)
         self.add_args()
@@ -31,15 +9,16 @@ class Config(BaseConfig):
 
     def add_args(self, **kwargs):
         self.batch_size = 1
-        self.num_epochs = 100
+        self.num_epochs = 30
 
         self.loss_type = "CrossEntropyLoss"
 
+       
         self.checkpoint_dir = "/kaggle/working/checkpoints/IEMOCAP"
 
         self.model_type = "MemoCMT"
 
-        self.text_encoder_type = "bert"  # [bert, roberta]
+        self.text_encoder_type = "bert"
         self.text_encoder_dim = 768
         self.text_unfreeze = False
 
@@ -47,19 +26,18 @@ class Config(BaseConfig):
         self.audio_encoder_dim = 768
         self.audio_unfreeze = False
 
-        self.fusion_dim: int = 768
+        self.fusion_dim = 768
 
         # Dataset
-        self.data_name: str = "IEMOCAP"
-        self.data_root: str = "working/dataset/IEMOCAP_preprocessed"
-        self.data_valid: str = "val.pkl"
-        self.text_max_length: int = 297
-        self.audio_max_length: int = 128000  # 160220
+        self.data_name = "IEMOCAP"
+       
+        self.data_root = "/kaggle/working/prem_memocmtmail/IEMOCAP_preprocessed"
+        self.data_valid = "val.pkl"
+        self.text_max_length = 297
+        self.audio_max_length = 128000
 
         # Config name
-        self.name = (
-            f"{self.model_type}_{self.text_encoder_type}_{self.audio_encoder_type}"
-        )
+        self.name = f"{self.model_type}_{self.text_encoder_type}_{self.audio_encoder_type}"
 
         for key, value in kwargs.items():
             setattr(self, key, value)
